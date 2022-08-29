36f36f90b43e170d494f54d572cb4bbbbe35fdf3
alexandr-shyshchuk <99428558+alexandr-shyshchuk@users.noreply.github.com>
2022-08-29 11:50:04 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-ds_set = {
-    'token': 'MTAxMzA0Mzc2Mzg5Mjk4NTkxOQ.GJFGYo.0c-6iF7Z8kVtie3ZN9eLUJ5_DoOcE_66xiFD5U',
-    'prefix': '?'
-}
-
-tg_token = '5715783737:AAFwVbnBSgYVIGbk62sOOoZS14Tm8GlUfVg'
